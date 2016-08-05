98f91639884e985f548358f6766009c591be0cb2
jollyKuma <jollyrilakkuma@gmail.com>
2016-08-05 16:21:25 +0800

    Done with Registration

--- app/views/account/register.blade.php
+++ app/views/account/register.blade.php
@@ -1,20 +1,36 @@
+<!@extends('account.layout')
+@section('css')
+    @parent
+    <style>
+        body {
+            background-color: #DADADA;
+            background-image: url('public/img/8.jpg')   !important;
+            background-repeat: no-repeat;
+            height: auto;
+            max-width: 100%;
+            background-size: cover;
+            top: 0;
+            left: 0;
+        }
+        body > .grid {
+            height: 100%;
+        }
+        .image {
+            margin-top: -100px;
+        }
 
-
-
-@section('title')
-    <title>Register - MaidFinder</title>
+    </style>
 @stop
-@extends('account.layout')
 @section('content')
 
   <div class="ui  column centered grid">
   <div class="column">
       &nbsp;
   </div>
-  <div class="four column centered row ui  middle aligned aligned center aligned grid very relaxed stackable grid">
+  <div class="four column centered row ui  middle aligned aligned center aligned grid very relaxed stackable grid ">
     <div class="column">
-        <h2 class="ui teal image header">
-                <img src="{{ asset('public/semantic/assets/img/icon.png') }}" class="image">
+        <h2 class="ui blue image header">
+                <img src="{{ asset('public/semantic/assets/img/icon5.png') }}" class="ui tiny image">
                 <span class="content">
                 Register with Email
                 </span>
@@ -79,7 +95,7 @@
                     <input type="password" name="confirm" placeholder=" Confirm Password">
                   </div>
                 </div>
-                  <input type="submit"  class="ui fluid large teal submit button" value="Submit" />
+                  <input type="submit"  class="ui fluid large teal submit button" value="Submit" style="background-color:#d0c38a" />
               </div>
               <div class="ui error message"></div>
 
